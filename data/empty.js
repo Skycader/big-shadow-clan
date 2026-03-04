f5dbd6dabd5b4bdd199905d5546c4d5666e759bb
Skycader <skycader@yandex.ru>
2026-03-05 02:49:26 +0300

    himera

---
+++ data/empty.js
@@ -0,0 +1,43 @@
+const HIMERA_PROJECT = {
+  tabName: "tabName",
+  treeTitle: "ЗРК <<Химера>>",
+  costBase: 500,
+  maxLevelPerSkill: 10,
+  skills: [
+    {
+      chapterTitle: "Модули Танка",
+      perks: [
+        {
+          id: "id",
+          name: "name",
+          description: "desc",
+        },
+      ],
+    },
+    {
+      chapterTitle: "Chapter 2",
+      perks: [
+        {
+          id: "skill_id",
+          name: "name",
+          description: "desc",
+        },
+      ],
+    },
+  ],
+
+  ranks: [
+    {
+      minLevel: 0,
+      maxLevel: 30,
+      name: "name",
+      badge: "...",
+    },
+  ],
+  skillLevelDescriptions: {
+    skill_name: {
+      1: "",
+      10: "",
+    },
+  },
+};
